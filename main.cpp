414c8bd35b6cee06c2a0ab1d80cd7dd04a55a208
Jackson Silvey <silversilvey@hotmail.com>
2024-12-17 22:12:33 -0600

    adding in removecomponent

--- main.cpp
+++ main.cpp
@@ -3,88 +3,106 @@
 const int MAX = 12;
 const int SPARSE_FACTOR = 2;
 struct SparseSet {
-	int dense[MAX];
-	int sparse[MAX * SPARSE_FACTOR];
+  int dense[MAX];
+  int sparse[MAX * SPARSE_FACTOR];
 
-	int n = 0;
-	void add(int x) {
-		dense[n] = x;
-		sparse[x] = n;
-		n++;
-	}
+  int n = 0;
+  void add(int x) {
+    dense[n] = x;
+    sparse[x] = n;
+    n++;
+  }
 
-	void remove(int x) {
-		// dense = {3, 4, 5};
-		// sparse = {0,0,0,0,1,2}
-		int dense_location = sparse[x];
-		int last_element = dense[n-1];
+  void remove(int x) {
+    int dense_location = sparse[x];
+    int last_element = dense[n - 1];
 
-		if (x != last_element) {
-			dense[dense_location] = last_element;
-			sparse[last_element] = dense_location;
-		} 
+    if (x != last_element) {
+      dense[dense_location] = last_element;
+      sparse[last_element] = dense_location;
+    }
+    dense[n - 1] = 0;
+    sparse[x] = 0;
+    n--;
+  }
 
-		dense[n-1] = 0;
+  int contains(int x) {
+    return dense[sparse[x]];
+  }
 
-		sparse[x] = 0;
-		n--;
+  void print() {
+    std::cout << "Dense: ";
+    for (int i = 0; i < MAX; i++) {
+      std::cout << dense[i];
+    }
+    std::cout << std::endl;
 
-	}
-
-	void print() {
-		std::cout << "Dense: ";
-		for (int i=0; i<MAX;i++) {
-			std::cout << dense[i];
-		}
-		std::cout << std::endl;
-
-		
-		std::cout << "Sparse: ";
-		for (int i=0; i<MAX*SPARSE_FACTOR;i++) {
-			std::cout << sparse[i];
-		}
-		std::cout << std::endl;
-	}
+    std::cout << "Sparse: ";
+    for (int i = 0; i < MAX * SPARSE_FACTOR; i++) {
+      std::cout << sparse[i];
+    }
+    std::cout << std::endl;
+  }
 };
 
 struct Health {
-	int value;
-	Health(int value=0) : value(value) {}
+  int value;
+  Health(int value = 0) : value(value) {}
+  static const Health Invalid;
 };
 
-template <typename T>
-struct ComponentStorage {
-	SparseSet sparseSet;
-	T components[50];
+const Health Health::Invalid = Health(-1);
 
-	
-	void addComponent(int entityID, const T& component) {
-		//insert entity into sparseset
-		sparseSet.add(entityID);
-		int ent_index = sparseSet.sparse[entityID];
-		components[ent_index] = component;
-	}
+template <typename T> struct ComponentStorage {
+  SparseSet sparseSet;
+  T components[50];
 
-	T* getComponent(int entityID) {
-		int index = sparseSet.sparse[entityID];
-		return &components[index];
-	}
+  void addComponent(int entityID, const T &component) {
+    sparseSet.add(entityID);
+    int ent_index = sparseSet.sparse[entityID];
+    components[ent_index] = component;
+  }
+
+  void removeComponent(int entityID) {
+    int remove_index = sparseSet.sparse[entityID];
+    int last_valid_index = sizeof(sparseSet.dense) - 1;
+
+    if (remove_index != last_valid_index) {
+      components[remove_index] = std::move(components[last_valid_index]);
+
+      int last_entity_id = sparseSet.dense[last_valid_index];
+      sparseSet.sparse[last_entity_id] = remove_index;
+    }
+    sparseSet.remove(entityID);
+  }
+
+  T *getComponent(int entityID) {
+
+    if (!sparseSet.contains(entityID)) {
+      return nullptr;
+    }
+
+    int index = sparseSet.sparse[entityID];
+    return &components[index];
+  }
+
 };
-
-
-
-
-
 
 int main() {
-	SparseSet s1 = SparseSet{};
-	s1.add(2);
-	s1.add(3);
-	s1.add(9);
+  SparseSet s1 = SparseSet{};
+  s1.add(2);
+  s1.add(3);
+  s1.add(9);
 
-	ComponentStorage<Health> healthStore;
-	healthStore.addComponent(3, Health(50));
+  ComponentStorage<Health> healthStore;
+  healthStore.addComponent(3, Health(50));
 
-	std::cout << healthStore.getComponent(3);
+  std::cout << healthStore.getComponent(3) << std::endl;
 
+  healthStore.removeComponent(3);
+  if (healthStore.getComponent(3) != nullptr) {
+    std::cout << healthStore.getComponent(3)->value << std::endl;
+  } else {
+    std::cout << "component doesnt exist" << std::endl;
+  }
 }
