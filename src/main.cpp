d07a7c395842d325043c62408fff0503caa852e2
jksnxd <silversilvey@hotmail.com>
2024-12-18 22:19:02 -0600

    beginning my SDL journey

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,29 @@
+#include "SDL3/SDL_log.h"
 #include "components/health.cpp"
 #include "components/position.cpp"
 #include "game/scene.cpp"
 #include <SDL3/SDL.h>
 
 int main() {
-  Scene active_scene = Scene{};
+  /*Scene active_scene = Scene{};*/
+  /**/
+  /*active_scene.registry.addComponent(1, Position(5, 20, 4));*/
+  /*active_scene.registry.addComponent(1, Health(100));*/
+  /**/
+  /*active_scene.registry.addComponent(9, Position(100, 200, 2));*/
+  /*active_scene.registry.addComponent(9, Health(25));*/
 
-  active_scene.registry.addComponent(1, Position(5, 20, 4));
-  active_scene.registry.addComponent(1, Health(100));
+  SDL_Init(SDL_INIT_VIDEO);
 
-  active_scene.registry.addComponent(9, Position(100, 200, 2));
-  active_scene.registry.addComponent(9, Health(25));
+  SDL_Window *window =
+      SDL_CreateWindow("Title", 500, 600, SDL_WINDOW_FULLSCREEN);
 
-  SDL_Init(SDL_INIT_AUDIO);
+  if (window == NULL) {
+    SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n",
+                 SDL_GetError());
+    return 1;
+  }
+
+  SDL_DestroyWindow(window);
+  SDL_Quit();
 }
